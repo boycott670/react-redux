b5075f695546b9edae635e0fffdef47cea21c598
MARMAR Soufiane <soufiane.marmar@cegedim.com>
2019-11-05 18:22:52 +0100

    Second

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import Provider, {connect} from './Provider';
 import './App.css';
+
+const store = (function () {
+  let state = [1, 2, 3];
+
+  let inc = 0;
+  let subscribers = {};
+
+  return {
+    getState: () => state,
+    dispatch: ({type = 'UNKNOWN', payload} = {}) => {
+      if (type === 'ADD') {
+        state = [...state, 1];
+        for (let subscriber in subscribers) subscribers[subscriber]();
+      }
+    },
+    subscribe: (cb) => {
+      inc = inc + 1;
+      subscribers[inc] = cb;
+      return () => {
+        subscribers = {...subscribers, [inc]: () => {}};
+      };
+    }
+  };
+})();
+
+class Child extends Component {
+  render() {
+    return <button onClick={() => this.props.call()}>{this.props.size}</button>;
+  }
+}
+
+Child = connect(
+  state => ({size: (state || []).length}),
+  dispatch => ({call: () => dispatch({type: 'ADD', payload: 1})})
+)(Child);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <Child/>
+    </Provider>
   );
 }
 
---
+++ src/Provider.js
@@ -0,0 +1,50 @@
+import React, {createContext, Component, Children} from 'react';
+
+const ProviderContext = createContext();
+
+class Watcher extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			subscriber: props.subscribe(() => {
+				this.forceUpdate();
+			})
+		};
+	}
+
+	componentWillUnmount() {
+		this.state.subscriber();
+	}
+
+	render() {
+		return this.props.children;
+	}
+}
+
+export function connect(mapStateToProps = () => ({}), mapDispatchToProps = () => ({})) {
+	return Component => {
+		return (props = {}) => {
+			return (
+				<ProviderContext.Consumer>
+					{
+						store => (
+							<Watcher subscribe={store.subscribe}>
+								<Component {...mapStateToProps(store.getState())} {...mapDispatchToProps(store.dispatch)} {...props}/>
+								{store.getState()}
+							</Watcher>
+						)
+					}
+				</ProviderContext.Consumer>
+			);
+		};
+	};
+};
+
+export default function Provider ({store, children} = {}) {
+	return (
+		<ProviderContext.Provider value={store}>
+			{children}
+		</ProviderContext.Provider>
+	);
+};
